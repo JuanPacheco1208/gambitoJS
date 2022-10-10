09857d7b50f38666ec7ee91a16d3696307097dc4
Juan Pacheco <juandapagi@hotmail.com>
2022-10-10 16:38:19 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+let ajedrez = prompt("¿Qué jugada es d4 + c4?");
+
+if(ajedrez == "gambito") {
+
+    alert("Muy bien!");
+
+}else if (ajedrez == "gambito de dama") {
+
+    alert("Excelente!");
+
+} else{
+    alert("Es gambito de dama!")
+}
+
+
+let casillas = parseInt(prompt("Cuantas casillas tiene un tablero de ajedrez?"))
+
+
+for (let i = 1; i <= casillas; i++ ) {
+    if(i % 2 == 0){
+        console.log("64");
+    }
+
+}
+
+
+let user = {     // un objeto
+    name: "Juan",  // En la clave "name" se almacena el valor "Juan"
+    age: 25        // En la clave "age" se almacena el valor 25
+};
+
+let piezas = ["dama", "rey", "alfil", "peon", "caballo", "torre"]; //array
+console.log(dama)
+
+// metodo de busqueda arrays
+
+let arr = ["Gambito", "Pagina", "Juego"];
+
+arr.splice(1, 1); // desde el índice 1, remover 1 elemento
+
+alert( Gambito );
+
+
+let titulo = document.getElementsByClassName("titulo").value;
+console.log(titulo);
+
+console.dir(document);
